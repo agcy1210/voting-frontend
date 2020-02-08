af9ba233a6592953b0d43914ebb2ea1f9acf8b76
Aman Chaudhary <agcy7320@gmail.com>
2020-02-08 07:51:32 +0530

    hash function added

--- voting/views.py
+++ voting/views.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import Voter,Candidate
+import hashlib
 
 # Create your views here.
 
@@ -19,7 +20,19 @@ def authenticate(request):
             
     return render(request,'voting/voterIdAuthentication.html')
 
+def getHash(secret_msg, voter_id):
+    hashString = secret_msg + str(voter_id)
+    return hashlib.sha256(hashString.encode()).hexdigest()
 
+@login_required(login_url='accounts/login')
+def secret_msg(request):
 
+    if request.method == 'POST':
+        secret_msg = request.POST['secret_msg']
+        reference_number = request.POST['reference_number']
 
+    hashString = secret_msg+reference_number
+    hashKey = getHash(hashString)
+
+    return render(request,'voting/voterIdAuthentication.html')
 
